a0422af7a730fcc4513f0ac3e9df19a50084c19d
xuejiangtao@99 <15503595314@163.com>
2021-03-22 21:02:52 +0800

    exit:1.新增business模块      2.新增大章表

---
+++ doc/db/all.sql
@@ -0,0 +1,8 @@
+-- 大章
+drop table if exists `chapter`;
+create table `chapter` (
+                           `id` char(8) not null comment 'id',
+                           `course_id` char(8) comment '课程id',
+                           `name` varchar(50) comment '名称',
+                           primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='大章';
