2cef5cbfb925c79a70caa89090960256f1121152
xuejiangtao@99 <15503595314@163.com>
2021-03-22 22:37:04 +0800

    exit:1.新增Chapter.vue页面及页面假数据

--- admin/src/router.js
+++ admin/src/router.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import Login from './views/Login';
 import Admin from './views/Admin';
 import Welcome from './views/admin/Welcome'
+import Chapter from './views/admin/Chapter'
 
 
 Vue.use(VueRouter)
@@ -24,6 +25,9 @@ export default new VueRouter({
         children:[{
             path:'welcome',
             component:Welcome
+        },{
+            path:"chapter",
+            component:Chapter
         }]
     }]
 })
