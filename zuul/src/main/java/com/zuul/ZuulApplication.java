f04f57ec23f45d7a38c944c7788e9e2848c184ba
xuejiangtao@99 <15503595314@163.com>
2021-03-23 11:54:07 +0800

    exit: 1.将cors放入gateWay       2.更改gateWay配置

--- zuul/src/main/java/com/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/zuul/ZuulApplication.java
@@ -6,7 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -24,6 +29,23 @@ public class ZuulApplication {
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！！");
         LOG.info("Zuul地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
+    }
+
+    //gateWay增加跨域配置
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(Boolean.TRUE);
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setMaxAge(3600L);
+
+        org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(source);
 
     }
 }
