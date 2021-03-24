56378d9398cffece01f8846a90a44778557279af
xuejiangtao@99 <15503595314@163.com>
2021-03-24 17:27:19 +0800

    exit 1.前台添加日志

--- admin/src/main.js
+++ admin/src/main.js
@@ -5,6 +5,16 @@ import axios from 'axios'
 
 Vue.prototype.$ajax = axios;
 
+axios.interceptors.request.use(function (config){
+      console.log("请求"+config)
+  return config;
+},error => {})
+
+axios.interceptors.response.use(function (response){
+  console.log("返回结果"+response)
+  return response;
+},error => {})
+
 Vue.config.productionTip = false
 
 new Vue({
