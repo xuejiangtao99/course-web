782451f87261f5d491fc3f786a7149fb187cf0b5
xuejiangtao@99 <15503595314@163.com>
2021-03-25 15:27:15 +0800

    exit 1.代码生成器和mybatis-generator整合

--- generator/src/main/java/com/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/generator/server/ServerGenerator.java
@@ -2,7 +2,13 @@ package com.generator.server;
 
 import com.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,10 +19,32 @@ public class ServerGenerator {
     static String toPath = "server\\src\\main\\java\\com\\server\\service\\impl\\";
     static String toServicePath = "server\\src\\main\\java\\com\\server\\service\\";
     static String toControllerPath = MODULE+"\\src\\main\\java\\com\\"+MODULE+"\\controller\\admin\\";
-    public static void main(String[] args) throws IOException, TemplateException {
+    static String generatorConfigPath = "server\\src\\main\\resources\\generator\\generatorConfig.xml";
+    public static void main(String[] args) throws IOException, TemplateException, DocumentException {
         Map<String,Object> map = new HashMap<>();
-        String Domain = "Section";
-        String domain = "section";
+//        String Domain = "Section";
+       // String domain = "section";
+        //只生成配置文件中第一个table节点
+        File file = new File(generatorConfigPath);
+        SAXReader reader=new SAXReader();
+        //读取xml文件到Document中
+        Document doc = reader.read(file);
+        //获取xml文件的根节点
+        Element rootElement = doc.getRootElement();
+        //读取context节点
+        Element context = rootElement.element("context");
+        //定义一个Element用于遍历
+        Element tableElement;
+        //取的第一个”table"的节点
+        tableElement=context.elementIterator("table").next();
+        String Domain = tableElement.attributeValue("domainObjectName"); //读取属性
+        String tableName = tableElement.attributeValue("tableName");
+//        String tableNameCn = DbUtil.getTableComment(tableName);
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        System.out.println("表："+tableElement.attributeValue("tableName"));
+        System.out.println("Domain："+tableElement.attributeValue("domainObjectName"));
+
+
         String module = MODULE;
 
         String tableNameCn ="+"+"小节"+"";
@@ -34,6 +62,14 @@ public class ServerGenerator {
         //生成Controller代码
         FreemarkerUtil.initConfig("controller.ftl");
         FreemarkerUtil.generator(toControllerPath+Domain+"Controller.java",map);
+
+
+        //todo
+        //生成Dto层代码
+
+
+
+
 
     }
 }
